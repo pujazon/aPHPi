36c2b855eb230f70e5e97f2f3cccd7786d59ec60
Daniel <danielpujazonbonet@gmail.com>
2018-05-08 22:38:30 +0200

    Fix

--- index.php
+++ index.php
@@ -3,50 +3,17 @@
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
 
+//Esto lo que hace es cuando hay el Submit 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
     $nameErr = "Name is required";
   } else {
-    $name = test_input($_POST["name"]);
+    //$name = test_input($_POST["name"]);
+    $name = "Hardcoded things"
     // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
-  /*
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL"; 
-    }
-  }
 
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
   }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  
-  }
-  */
 }
 
 function test_input($data) {
@@ -212,8 +179,7 @@ function test_input($data) {
 							</a>
 						</li>
 						<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-						  Name: <input type="text" name="name" value="<?php echo $name;?>">
-						  <span class="error">* <?php echo $nameErr;?></span>
+						  <input name="c1" value="c1">
 						  <br><br>
 						  <input type="submit" name="submit" value="Submit">  
 						</form>
